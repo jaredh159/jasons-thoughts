ee3f7fa3a38ce0745e5f15e0b6d2aaa87818923d
Jared Henderson <jaredh159@users.noreply.github.com>
2022-07-16 14:12:22 -0400

    initial commit

---
+++ src/lib/api.ts
@@ -0,0 +1,12 @@
+import type { Post } from "../types";
+import { bucketSlug, readKey } from "./config";
+
+export async function getAllPosts(): Promise<Post[]> {
+  const endpoint = `https://api.cosmicjs.com/v2/buckets`;
+  const query = encodeURIComponent(`{"type":"posts"}`);
+  const url = `${endpoint}/${bucketSlug}/objects?&query=${query}&read_key=${readKey}`;
+  const response = await fetch(url);
+  const objs = await response.json();
+  const allPosts: Array<Post> = objs.objects;
+  return allPosts;
+}
---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type Post = {
+  id: string;
+  title: string;
+  slug: string;
+  content: string;
+  bucket: string;
+  created: string;
+  created_at: string;
+  created_by: string;
+  modified_at: string;
+  published_at: string;
+  type: "posts";
+};

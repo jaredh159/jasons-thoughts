338802acf76e429532de7c829dacea4ad6c154f8
Jared Henderson <jaredh159@users.noreply.github.com>
2022-09-26 10:11:44 -0400

    add atom to rss again

--- src/lib/podcast.ts
+++ src/lib/podcast.ts
@@ -12,6 +12,7 @@ export function podcastXml(posts: Array<Post<Lang>>): string {
   <rss
     version="2.0"
     xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"
+    xmlns:atom="http://www.w3.org/2005/Atom"
     xmlns:content="http://purl.org/rss/1.0/modules/content/">
     <channel>
       <atom:link

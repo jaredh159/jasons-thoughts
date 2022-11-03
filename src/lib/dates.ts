9645c95b196a8c0ddf6d25a8e74c0ff11c3f6ff1
Kiah <miciahjohnhenderson@gmail.com>
2022-11-02 22:06:27 -0400

    made homepage

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -27,3 +27,25 @@ export const spanishMonths = [
   'Noviembre',
   'Diciembre',
 ];
+
+const rtf = new Intl.RelativeTimeFormat(`en`, { numeric: `auto` });
+
+export function relativeTime(isoOrDate: Date | string): string {
+  const date = typeof isoOrDate === `string` ? new Date(isoOrDate) : isoOrDate;
+  const now = new Date();
+  const diff = now.getTime() - date.getTime();
+  const UNITS = {
+    year: 24 * 60 * 60 * 1000 * 365,
+    month: (24 * 60 * 60 * 1000 * 365) / 12,
+    day: 24 * 60 * 60 * 1000,
+    hour: 60 * 60 * 1000,
+    minute: 60 * 1000,
+    second: 1000,
+  } as const;
+  for (const [unit, num] of Object.entries(UNITS)) {
+    if (Math.abs(diff) > num || unit === `second`) {
+      return rtf.format(-Math.round(diff / num), unit as any);
+    }
+  }
+  return `just now`;
+}

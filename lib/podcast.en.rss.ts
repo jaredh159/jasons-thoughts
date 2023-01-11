7068cfc4312f222bd660530ff7c9dad4e6994a19
Kiah <miciahjohnhenderson@gmail.com>
2023-01-11 14:40:08 -0500

    TEMP: moved .rss files

--- lib/podcast.en.rss.ts
+++ lib/podcast.en.rss.ts
@@ -1,5 +1,5 @@
-import { getAllPosts } from '../lib/getAllPosts';
-import { podcastXml } from '../lib/podcast';
+import { getAllPosts } from './getAllPosts';
+import { podcastXml } from './podcast';
 
 export const get = async () => {
   const englishPosts = (await getAllPosts()).map((dual) => dual.en);
